9bb730a0e038f8f3ac4e4fb11fac44dcbcfc27bc
kavisharma05 <iamkavishsharma@gmail.com>
2025-06-04 07:22:58 +0530

    adjusting padding for the fixed header

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          <main>{children}</main>
+          <main className="pt-20">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
